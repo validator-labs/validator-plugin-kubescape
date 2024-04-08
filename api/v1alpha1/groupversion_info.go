validator-labs/validator-plugin-kubescape

commit 7cf23167b3d9cf95b88547b928869282e4d09106

fix: v1 to v1alpha1

api/v1alpha1/groupversion_info.go
@@ -17,7 +17,7 @@ limitations under the License.
 // Package v1 contains API Schema definitions for the validation v1 API group
 // +kubebuilder:object:generate=true
 // +groupName=validation.spectrocloud.labs
-package v1
+package v1alpha1
 
 import (
 	"k8s.io/apimachinery/pkg/runtime/schema"

api/v1alpha1/kubescapevalidator_types.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package v1
+package v1alpha1
 
 import (
 	"fmt"

config/crd/bases/validation.spectrocloud.labs_kubescapevalidators.yaml
@@ -14,7 +14,7 @@ spec:
     singular: kubescapevalidator
   scope: Namespaced
   versions:
-  - name: v1
+  - name: v1alpha1
     schema:
       openAPIV3Schema:
         description: KubescapeValidator is the Schema for the kubescapevalidators

internal/controller/kubescapevalidator_controller.go
@@ -32,8 +32,8 @@ import (
 
 	"github.com/go-logr/logr"
 	kubevuln "github.com/kubescape/kubevuln/repositories"
-	kubescapevalidatorv1 "github.com/spectrocloud-labs/validator-plugin-kubescape/api/v1"
-	validationv1 "github.com/spectrocloud-labs/validator-plugin-kubescape/api/v1"
+	kubescapevalidatorv1 "github.com/spectrocloud-labs/validator-plugin-kubescape/api/v1alpha1"
+	validationv1 "github.com/spectrocloud-labs/validator-plugin-kubescape/api/v1alpha1"
 	"github.com/spectrocloud-labs/validator-plugin-kubescape/internal/constants"
 	"github.com/spectrocloud-labs/validator-plugin-kubescape/internal/validators"
 	vapi "github.com/spectrocloud-labs/validator/api/v1alpha1"

internal/validators/kubescape.go
@@ -7,7 +7,7 @@ import (
 	"github.com/go-logr/logr"
 	kubevuln "github.com/kubescape/kubevuln/repositories"
 	kubescapev1 "github.com/kubescape/storage/pkg/apis/softwarecomposition/v1beta1"
-	validationv1 "github.com/spectrocloud-labs/validator-plugin-kubescape/api/v1"
+	validationv1 "github.com/spectrocloud-labs/validator-plugin-kubescape/api/v1alpha1"
 	"github.com/spectrocloud-labs/validator-plugin-kubescape/internal/constants"
 	vapi "github.com/spectrocloud-labs/validator/api/v1alpha1"
 	"github.com/spectrocloud-labs/validator/pkg/types"

PROJECT
@@ -15,6 +15,6 @@ resources:
   domain: spectrocloud.labs
   group: validation
   kind: KubescapeValidator
-  path: github.com/spectrocloud-labs/validator-plugin-kubescape/api/v1
-  version: v1
+  path: github.com/spectrocloud-labs/validator-plugin-kubescape/api/v1alpha1
+  version: v1alpha1
 version: "3"

api/v1alpha1/zz_generated.deepcopy.go
@@ -19,7 +19,7 @@ limitations under the License.
 
 // Code generated by controller-gen. DO NOT EDIT.
 
-package v1
+package v1alpha1
 
 import (
 	runtime "k8s.io/apimachinery/pkg/runtime"

cmd/main.go
@@ -31,7 +31,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-	validationv1 "github.com/spectrocloud-labs/validator-plugin-kubescape/api/v1"
+	validationv1 "github.com/spectrocloud-labs/validator-plugin-kubescape/api/v1alpha1"
 	"github.com/spectrocloud-labs/validator-plugin-kubescape/internal/controller"
 	validatorv1alpha1 "github.com/spectrocloud-labs/validator/api/v1alpha1"
 	//+kubebuilder:scaffold:imports

commit 62fbc75da3218a10b58a81fd0462f4e926c5512e

fix: group version, namespace, vuln counter
- 7cf2316 broke install due to group version
- support separate namespace install for kubescape
- fix vulnerability counter

api/v1alpha1/groupversion_info.go
@@ -26,7 +26,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects
-	GroupVersion = schema.GroupVersion{Group: "validation.spectrocloud.labs", Version: "v1"}
+	GroupVersion = schema.GroupVersion{Group: "validation.spectrocloud.labs", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}

api/v1alpha1/kubescapevalidator_types.go
@@ -27,6 +27,9 @@ import (
 
 // KubescapeValidatorSpec defines the desired state of KubescapeValidator
 type KubescapeValidatorSpec struct {
+	//+kubebuilder:default=kubescape
+	Namespace string `json:"namespace,omitempty" yaml:"namespace,omitempty"`
+	// Global Severity Limit Rule
 	SeverityLimitRule SeverityLimitRule `json:"severityLimitRule,omitempty" yaml:"severityLimitRule,omitempty"`
 	// Global Ignore CVEs
 	IgnoredCVERule []string `json:"ignoredCVERule,omitempty" yaml:"ignoredCVERule,omitempty"`
@@ -42,7 +45,13 @@ func (r FlaggedCVE) Name() string {
 
 // Increase for every rule
 func (s KubescapeValidatorSpec) ResultCount() int {
-	count := 1
+	count := 0
+	if s.SeverityLimitRule != (SeverityLimitRule{}) {
+		count++
+	}
+	count += len(s.IgnoredCVERule)
+	count += len(s.FlaggedCVERule)
+
 	return count
 }
 

config/crd/bases/validation.spectrocloud.labs_kubescapevalidators.yaml
@@ -45,7 +45,11 @@ spec:
                 items:
                   type: string
                 type: array
+              namespace:
+                default: kubescape
+                type: string
               severityLimitRule:
+                description: Global Severity Limit Rule
                 properties:
                   critical:
                     type: integer

config/default/kustomization.yaml
@@ -1,5 +1,5 @@
 # Adds namespace to all resources.
-namespace: validator-plugin-kubescape-system
+namespace: validator
 
 # Value of this field is prepended to the
 # names of all resources, e.g. a deployment named

internal/controller/kubescapevalidator_controller.go
@@ -106,7 +106,7 @@ func (r *KubescapeValidatorReconciler) Reconcile(ctx context.Context, req ctrl.R
 		ValidationRuleErrors:  make([]error, 0, vr.Spec.ExpectedResults),
 	}
 
-	kubescape, err := kubevuln.NewAPIServerStorage("kubescape")
+	kubescape, err := kubevuln.NewAPIServerStorage(validator.Spec.Namespace)
 
 	if err != nil {
 		return ctrl.Result{RequeueAfter: time.Second * 120}, errors.New("cannot connect to kubescape api storage server, is kubescape operator installed?")
@@ -168,7 +168,7 @@ func buildValidationResult(validator *kubescapevalidatorv1.KubescapeValidator) *
 		},
 		Spec: vapi.ValidationResultSpec{
 			Plugin:          constants.PluginCode,
-			ExpectedResults: 1,
+			ExpectedResults: validator.Spec.ResultCount(),
 		},
 	}
 }

internal/validators/kubescape.go
@@ -55,15 +55,20 @@ func (n *KubescapeService) Manifests() ([]kubescapev1.VulnerabilityManifest, err
 func (n *KubescapeService) ReconcileSeverityRule(nn ktypes.NamespacedName, rule validationv1.SeverityLimitRule, ignoredCVEs []string, manifests []kubescapev1.VulnerabilityManifest) (*types.ValidationRuleResult, error) {
 	vr := buildValidationResult(rule, constants.ValidationTypeSeverity)
 
-	zero := 0
+	critical := 0
+	high := 0
+	medium := 0
+	low := 0
+	unknown := 0
+	negligible := 0
 
 	foundVulns := validationv1.SeverityLimitRule{
-		Critical:   &zero,
-		High:       &zero,
-		Medium:     &zero,
-		Low:        &zero,
-		Unknown:    &zero,
-		Negligible: &zero,
+		Critical:   &critical,
+		High:       &high,
+		Medium:     &medium,
+		Low:        &low,
+		Unknown:    &unknown,
+		Negligible: &negligible,
 	}
 
 	uniqueCVEs := make(map[string]bool)